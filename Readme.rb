e9e3610e0b0ea3a3a7f253428afcbd0558a510bb
PhantomWorld <heider_yagami@hotmail.com>
2022-10-10 20:43:01 -0300

    Readme

---
+++ Readme.rb
@@ -0,0 +1,4 @@
+Um jogo baseado em jogos clássicos como mario party do nintendo 64 e yu gi
+oh forbidden memories em questão de jogabilidade mas que o universo seja
+livre para os desenvolvedores, um jogo de tabuleiro e em turno, integrado
+com minigame de cartas, para proporcionar diversão e nostalgia aos players;
